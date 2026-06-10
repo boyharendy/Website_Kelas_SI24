d602b618903a68bff5b377112b53e5ba75cc213d
Boy Simamora <boyharendy321@gmail.com>
2026-06-10 14:30:21 +0700

    chore: update drizzle config for turso

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   schema: './backend/db/schema.js',
   out: './backend/db/migrations',
-  dialect: 'sqlite',
+  dialect: 'turso',
   dbCredentials: {
-    url: './backend/db/sqlite.db',
+    url: process.env.TURSO_DATABASE_URL || 'file:backend/db/sqlite.db',
+    authToken: process.env.TURSO_AUTH_TOKEN,
   },
 });
